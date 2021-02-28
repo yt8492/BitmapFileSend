c6bfb7aaa2a11451f75785a26063f7789fdbd8b5
yt8492 <tmymyut2000@gmail.com>
2021-02-28 11:51:12 +0900

    ドクロ差し替え

--- app/src/main/java/com/yt8492/bitmapfilesend/Constants.kt
+++ app/src/main/java/com/yt8492/bitmapfilesend/Constants.kt
@@ -3,5 +3,5 @@ package com.yt8492.bitmapfilesend
 import java.util.*
 
 object Constants {
-    val APP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    val APP_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 }
